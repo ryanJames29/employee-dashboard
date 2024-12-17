d141dab05f6d1361852a7bc03eb2863dfec31350
Ryan James <ryanj29@colostate.edu>
2024-12-17 11:46:02 -0700

    Initial commit: Flask + React project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React 18</h1>
+      <p>This app is manually set up without create-react-app conflicts.</p>
+    </div>
+  );
+}
+
+export default App;
